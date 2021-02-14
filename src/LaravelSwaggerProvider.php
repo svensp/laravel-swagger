9aab10e95b99df930377d05613d8d92c0829da53
Sven Speckmaier <sps@ipunkt.biz>
2021-02-14 21:47:00 +0100

    publish config

--- src/LaravelSwaggerProvider.php
+++ src/LaravelSwaggerProvider.php
@@ -13,10 +13,23 @@ class LaravelSwaggerProvider extends \Illuminate\Support\ServiceProvider
     public function boot()
     {
 
+        $this->registerCommands();
+        $this->registerPublishedFFiles();
+    }
+
+    private function registerCommands(): void
+    {
         if ($this->app->runningInConsole()) {
             $this->commands([
                 GenerateOpenApiCommand::class,
             ]);
         }
+    }
+
+    private function registerPublishedFFiles(): void
+    {
+        $this->publishes([
+            __DIR__ . '/../config/open-api.php' => config_path('open-api.php'),
+        ]);
     }
 }
