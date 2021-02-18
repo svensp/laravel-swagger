15ea4623075fbf196f84f64512d0c5b3ad9245d7
Sven Speckmaier <sps@ipunkt.biz>
2021-02-18 23:14:35 +0100

    FilesystemApiDocIO make sure files are written, including with aliases

--- src/Filesystem/FileSystemApiDocIO.php
+++ src/Filesystem/FileSystemApiDocIO.php
@@ -21,7 +21,9 @@ class FileSystemApiDocIO implements ApiDocIO
 
         $existingApiDoc = $this->parseExisting();
 
-        $modifier($existingApiDoc);
+        $modifiedApiDoc = $modifier($existingApiDoc);
+
+        $this->writeModified($modifiedApiDoc);
     }
 
     private function parseExisting(): array
@@ -34,10 +36,16 @@ class FileSystemApiDocIO implements ApiDocIO
         }
     }
 
+    private function writeModified($modifiedData)
+    {
+        $newYaml = $this->convertToYaml($modifiedData);
+        $this->writeToFile($newYaml);
+    }
+
     private function readYamlContentFromFile()
     {
         $pathWithAliasesResolved = $this->applyAliases($this->pathToRead);
-        $yamlContent = file_get_contents($pathWithAliasesResolve);
+        $yamlContent = file_get_contents($pathWithAliasesResolved);
         if ($yamlContent === false) {
             throw new ReadFailedException();
         }
@@ -47,6 +55,17 @@ class FileSystemApiDocIO implements ApiDocIO
     private function parseYaml(): array
     {
         return Yaml::parse($this->yamlContent);
+    }
+
+    private function convertToYaml($data)
+    {
+        return Yaml::dump($data, 8, 2);
+    }
+
+    private function writeToFile($newYaml)
+    {
+        $pathWithAliasesResolved = $this->applyAliases($this->pathToRead);
+        file_put_contents($pathWithAliasesResolved, $newYaml);
     }
 
     public function setAlias(string $name, string $aliasPath)
--- tests/Unit/FilesystemApiDocIOTest.php
+++ tests/Unit/FilesystemApiDocIOTest.php
@@ -3,6 +3,7 @@
 use LaravelSwagger\Filesystem\FileSystemApiDocIO;
 use LaravelSwagger\OpenApi\ApiDocIO;
 use LaravelSwaggerTest\TestCase;
+use Symfony\Component\Yaml\Yaml;
 
 /**
  * Class FilesystemApiDocIO
@@ -32,9 +33,10 @@ class FilesystemApiDocIOTest extends TestCase
     /**
      * @test
      */
-    public function can_update_via_full_path()
+    public function update_via_full_path_receives_correct_previous_data()
     {
-        $this->updateAndAssert(__DIR__.'/data/test.yml', function ($existingApiDoc) {
+        $this->prepareFile(__DIR__.'/data/test.yml');
+        $this->assertExistingDataOnUpdate(__DIR__.'/data/test.yml', function ($existingApiDoc) {
             $this->assertExistingApiDocFound($existingApiDoc);
         });
     }
@@ -42,16 +44,56 @@ class FilesystemApiDocIOTest extends TestCase
     /**
      * @test
      */
-    public function can_update_via_alias_in_path()
+    public function update_via_alias_receives_correct_previous_data()
     {
+        $this->prepareFile(__DIR__.'/data/test.yml');
+
         $this->apiDocIO->setAlias('@', __DIR__.'/data');
 
-        $this->updateAndAssert('@/test.yml', function ($existingApiDoc) {
+        $this->assertExistingDataOnUpdate('@/test.yml', function ($existingApiDoc) {
             $this->assertExistingApiDocFound($existingApiDoc);
         });
     }
 
-    private function updateAndAssert($path, callable $asserts)
+    /**
+     * @test
+     */
+    public function update_via_alias_updates_correct_file()
+    {
+        $this->prepareFile(__DIR__.'/data/test.yml');
+
+        $this->apiDocIO->setAlias('@', __DIR__.'/data');
+
+        $this->apiDocIO->update('@/test.yml', function($previousData) {
+            $previousData['was-run'] = 'run';
+            return $previousData;
+        });
+
+        $this->assertFileUpdated(__DIR__.'/data/test.yml', function($data) {
+            $this->assertArrayHas('was-run', $data);
+            $this->assertArrayEquals('run', 'was-run', $data);
+        });
+    }
+
+    /**
+     * @test
+     */
+    public function update_via_full_path_updates_correct_file()
+    {
+        $this->prepareFile(__DIR__.'/data/test.yml');
+
+        $this->apiDocIO->update(__DIR__.'/data/test.yml', function($previousData) {
+            $previousData['was-run'] = 'run';
+            return $previousData;
+        });
+
+        $this->assertFileUpdated(__DIR__.'/data/test.yml', function($data) {
+            $this->assertArrayHas('was-run', $data);
+            $this->assertArrayEquals('run', 'was-run', $data);
+        });
+    }
+
+    private function assertExistingDataOnUpdate($path, callable $asserts)
     {
         $called = false;
         $this->apiDocIO->update($path, function ($previousData) use (&$called, $asserts) {
@@ -61,10 +103,21 @@ class FilesystemApiDocIOTest extends TestCase
         $this->assertTrue($called);
     }
 
+    private function assertFileUpdated($path, callable $asserts)
+    {
+        $resultData = Yaml::parse(file_get_contents($path));
+        $asserts($resultData);
+    }
+
     private function assertExistingApiDocFound($existingApiDoc)
     {
         $this->assertArrayEquals('cookies', 'test.data', $existingApiDoc);
         $this->assertArrayEquals('entry1', 'test.array.0', $existingApiDoc);
         $this->assertArrayEquals('entry2', 'test.array.1', $existingApiDoc);
+    }
+
+    private function prepareFile(string $path)
+    {
+        copy(__DIR__.'/data/template.yml', $path);
     }
 }
